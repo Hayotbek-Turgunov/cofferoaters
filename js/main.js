31df00e7b16182daa1ef43575fd0441ff054f2fc
Haytbek-Turgunov <turgunovhayotbek12@gmail.com>
2023-08-09 20:01:07 +0500

    Add initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var modifiers = {
+    siteHeaderOpen: 'site-header--open',
+    formGroupOpen: 'form-group--open'
+  };
+
+
 // SITE-HEADER
 var elSiteHeader = document.querySelector('.site-header');
 var elSiteHeaderToggler = elSiteHeader.querySelector('.js-site-header-toggler');
